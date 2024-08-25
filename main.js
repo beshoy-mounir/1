06246f855e496ea42f0f9e220f0aeda587858b10
Beshoy <beshoy_mounir22@yahoo.com>
2024-08-25 04:16:34 +0300

    Added condition Integer between 0-->99

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ let input = document.getElementById("inputText");
 btn.addEventListener("click", () => {
     if (isNaN(input.value) || input.value == "") {
         h1.innerHTML = `Invaild Input`;
+    } else if (
+        (!isNaN(input.value) && input.value > 99) ||
+        input.value % 1 !== 0
+    ) {
+        h1.innerHTML = `The Range is Between 0 to 99 "Integer"`;
     } else {
         h1.innerHTML = ``;
         fetch("https://api.imgflip.com/get_memes")
